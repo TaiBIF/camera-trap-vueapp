a2551abe9d7c3807516cd941f320bb7d86ec87ae
diabloevagto <diabloevagto@gmail.com>
2019-04-04 21:55:40 +0800

    feat: api 檔案結構

--- src/service/index.js
+++ src/service/index.js
@@ -1,1 +1,7 @@
 export * from './modules/account';
+export * from './modules/annotations';
+export * from './modules/config';
+export * from './modules/notifications';
+export * from './modules/projects';
+export * from './modules/site';
+export * from './modules/upload';
