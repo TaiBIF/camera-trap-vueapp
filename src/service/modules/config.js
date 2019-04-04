124ad3e432a541b15afce31459b1f06b4f83ded3
diabloevagto <diabloevagto@gmail.com>
2019-04-04 22:01:15 +0800

    refactor: remove `/api/v1`

--- src/service/modules/config.js
+++ src/service/modules/config.js
@@ -3,7 +3,7 @@ import fetchWrap from '@/utils/fetch';
 // https://github.com/TaiBIF/camera-trap-api/wiki/API-v1-Document#get-project-areas
 const getProjectAreas = async () => {
   const res = await fetchWrap({
-    url: '/api/v1/project-areas',
+    url: '/project-areas',
     method: 'GET',
   });
   return res;
