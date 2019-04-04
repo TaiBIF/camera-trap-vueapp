09a8da9900d6b0c247ee542ba4143edc36f52449
diabloevagto <diabloevagto@gmail.com>
2019-04-05 00:07:58 +0800

    feat: 取得計畫清單及計畫詳細資料 api

--- src/service/modules/projects.js
+++ src/service/modules/projects.js
@@ -1,1 +1,21 @@
-export {};
+import fetchWrap from '@/utils/fetch';
+
+// https://github.com/TaiBIF/camera-trap-api/wiki/API-v1-Document#get-projects
+const getProjects = async () => {
+  const res = await fetchWrap({
+    url: '/projects',
+    method: 'GET',
+  });
+  return res;
+};
+
+// https://github.com/TaiBIF/camera-trap-api/wiki/API-v1-Document#get-projectsprojectid
+const getProjectDetail = async id => {
+  const res = await fetchWrap({
+    url: `/projects/${id}`,
+    method: 'GET',
+  });
+  return res;
+};
+
+export { getProjects, getProjectDetail };
