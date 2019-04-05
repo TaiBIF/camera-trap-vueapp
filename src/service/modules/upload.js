9bd394aa957d304bf574adeea7a6d6883793852d
diabloevagto <diabloevagto@gmail.com>
2019-04-05 17:46:48 +0800

    feat: upload file

--- src/service/modules/upload.js
+++ src/service/modules/upload.js
@@ -1,1 +1,13 @@
-export {};
+import { fetchUpload } from '@/utils/fetch';
+
+const uploadFile = async (type, body) => {
+  const url = `/api/v1/files?type=${type}`;
+  const data = await fetchUpload({
+    url,
+    method: 'POST',
+    body,
+  });
+  return data;
+};
+
+export { uploadFile };
