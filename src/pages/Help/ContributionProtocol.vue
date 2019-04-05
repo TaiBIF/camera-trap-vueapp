247edda671934327e221f9a075eb7b824c1b0e39
oahehc <oahehc@gmail.com>
2019-04-05 16:42:17 +0800

    refactor

--- src/pages/Help/ContributionProtocol.vue
+++ src/pages/Help/ContributionProtocol.vue
@@ -1,40 +1,35 @@
 <template>
   <div class="container page-article">
-    <div v-if="loading">
-      <p>Loading</p>
-    </div>
-    <div v-if="prose">
+    <div>
       <h1 class="text-green">
-        {{ prose.title }}
+        {{ title }}
       </h1>
       <p class="text-gray">
-        {{ prose.description }}
+        {{ description }}
       </p>
       <article>
-        <prose-content></prose-content>
+        <mark-down></mark-down>
       </article>
-      <div class="text-right text-gray">最後更新日期：{{ prose.date }}</div>
+      <div v-if="date" class="text-right text-gray">
+        最後更新日期：{{ date }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import ProseContent from '@/prose/contribution-protocol.md';
+import markdown from '@/prose/contribution-protocol.md';
 
 export default {
   name: 'ContributionProtocol',
   components: {
-    'prose-content': ProseContent,
+    MarkDown: markdown,
   },
   data() {
     return {
-      loading: false,
-      prose: {
-        path: 'contribution-protocol',
-        title: 'Camera Trap 自動相機資料平台貢獻協議',
-        description: '',
-        date: '2018-11-01',
-      },
+      title: 'Camera Trap 自動相機資料平台貢獻協議',
+      description: '',
+      date: '2018-11-01',
     };
   },
 };
