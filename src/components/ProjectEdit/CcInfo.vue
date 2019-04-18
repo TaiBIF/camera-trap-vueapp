06f805ce62f606f234d0d1771ff021ba008cc071
diabloevagto <diabloevagto@gmail.com>
2019-04-19 02:05:15 +0800

    feat: ch2.3 component

--- src/components/ProjectEdit/CcInfo.vue
+++ src/components/ProjectEdit/CcInfo.vue
@@ -1,77 +1,217 @@
 <template>
   <div v-if="!!project">
-    <label>詮釋資料: </label>
-    <input
-      type="radio"
-      value="cc0"
-      :checked="project.interpretiveDataLicense === 'cc0'"
-      @change="changeProps('interpretiveDataLicense', $event.target.value)"
-    />
-    <label>無著作權 (CC0)</label>
-    <input
-      type="radio"
-      value="by"
-      :checked="project.interpretiveDataLicense === 'by'"
-      @change="changeProps('interpretiveDataLicense', $event.target.value)"
-    />
-    <label>姓名標示</label>
-    <input
-      type="radio"
-      value="by-nc"
-      :checked="project.interpretiveDataLicense === 'by-nc'"
-      @change="changeProps('interpretiveDataLicense', $event.target.value)"
-    />
-    <label>姓名標示-非商業性</label>
-    <br />
-    <label>鑑定資訊: </label>
-    <input
-      type="radio"
-      value="by"
-      :checked="project.identificationInformationLicense === 'by'"
-      @change="
-        changeProps('identificationInformationLicense', $event.target.value)
-      "
-    />
-    <label>姓名標示</label>
-    <br />
-    <label>影像資料: </label>
-    <input
-      type="radio"
-      value="cc0"
-      :checked="project.videoMaterialLicense === 'cc0'"
-      @change="changeProps('videoMaterialLicense', $event.target.value)"
-    />
-    <label>無著作權 (CC0)</label>
-    <input
-      type="radio"
-      value="by"
-      :checked="project.videoMaterialLicense === 'by'"
-      @change="changeProps('videoMaterialLicense', $event.target.value)"
-    />
-    <label>姓名標示</label>
-    <input
-      type="radio"
-      value="by-nc"
-      :checked="project.videoMaterialLicense === 'by-nc'"
-      @change="changeProps('videoMaterialLicense', $event.target.value)"
-    />
-    <label>姓名標示-非商業性</label>
-    <br />
-    <label>公開日期</label>
-    <input
-      type="date"
-      :value="formatTime(project.publishTime)"
-      @change="setProjectTime('publishTime', $event.target.value)"
-    />
+    <form class="form form-horizontal">
+      <div class="panel">
+        <div class="panel-heading">
+          <h4>創用CC授權許可</h4>
+        </div>
+        <div class="panel-body">
+          <p>
+            授權計畫內的詮釋資料、鑑定資訊與影像資訊讓其他遵守授權條款的用戶不必向您詢問許可，即能合法使用其中資料。「Camera
+            Trap 監測資料管理平台」使用您的創用 CC
+            授權內容，來與像是全球生物多樣性資訊機構（GBIF），一個專門編輯發佈全球生物多樣化訊息之國際政府間組織如此般的資料合作者共享內容。
+            了解<a href="http://creativecommons.tw/" target="_blank"
+              >創用 CC 授權內容</a
+            >。
+          </p>
+          <div class="form-group">
+            <label for="">詮釋資料：</label>
+            <div>
+              <div class="radio">
+                <input
+                  type="radio"
+                  id="for-data-1"
+                  v-model="project.interpretiveDataLicense"
+                  value="cc0"
+                  :checked="project.interpretiveDataLicense === 'cc0'"
+                />
+                <label for="for-data-1">
+                  <img
+                    src="/assets/common/cc-0.png"
+                    height="40px"
+                    srcset="/assets/common/cc-0@2x.png"
+                  />
+                  <span class="text">無著作權 (CC0)</span>
+                </label>
+              </div>
+              <div class="radio">
+                <input
+                  type="radio"
+                  id="for-data-2"
+                  v-model="project.interpretiveDataLicense"
+                  value="by"
+                  :checked="project.interpretiveDataLicense === 'by'"
+                />
+                <label for="for-data-2">
+                  <img
+                    src="/assets/common/cc-1.png"
+                    height="40px"
+                    srcset="/assets/common/cc-1@2x.png"
+                  />
+                  <span class="text">姓名標示</span>
+                </label>
+              </div>
+              <div class="radio">
+                <input
+                  type="radio"
+                  id="for-data-3"
+                  v-model="project.interpretiveDataLicense"
+                  value="by-nc"
+                  :checked="project.interpretiveDataLicense === 'by-nc'"
+                />
+                <label for="for-data-3">
+                  <img
+                    src="/assets/common/cc-2.png"
+                    height="40px"
+                    srcset="/assets/common/cc-2@2x.png"
+                  />
+                  <span class="text">姓名標示-非商業性</span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="">鑑定資訊：</label>
+            <div>
+              <div class="radio">
+                <input
+                  type="radio"
+                  id="for-info-1"
+                  v-model="project.identificationInformationLicense"
+                  value="by"
+                />
+                <label for="for-info-1">
+                  <img
+                    src="/assets/common/cc-1.png"
+                    height="40px"
+                    srcset="/assets/common/cc-1@2x.png"
+                  />
+                  <span class="text">姓名標示</span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="">影像資料：</label>
+            <div>
+              <div class="radio">
+                <input
+                  type="radio"
+                  id="for-img-1"
+                  v-model="project.videoMaterialLicense"
+                  value="cc0"
+                />
+                <label for="for-img-1">
+                  <img
+                    src="/assets/common/cc-0.png"
+                    height="40px"
+                    srcset="/assets/common/cc-0@2x.png"
+                  />
+                  <span class="text">無著作權 (CC0)</span>
+                </label>
+              </div>
+              <div class="radio">
+                <input
+                  type="radio"
+                  id="for-img-2"
+                  v-model="project.videoMaterialLicense"
+                  value="by"
+                />
+                <label for="for-img-2">
+                  <img
+                    src="/assets/common/cc-1.png"
+                    height="40px"
+                    srcset="/assets/common/cc-1@2x.png"
+                  />
+                  <span class="text">姓名標示</span>
+                </label>
+              </div>
+              <div class="radio">
+                <input
+                  type="radio"
+                  id="for-img-3"
+                  v-model="project.videoMaterialLicense"
+                  value="by-nc"
+                />
+                <label for="for-img-3">
+                  <img
+                    src="/assets/common/cc-2.png"
+                    height="40px"
+                    srcset="/assets/common/cc-2@2x.png"
+                  />
+                  <span class="text">姓名標示-非商業性</span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="form-group row">
+            <label for="" class="pl-3 required">公開日期：</label>
+            <div class="d-inline-block pl-3">
+              <div
+                class="input-group"
+                :class="{ 'is-invalid': !project.publishTime }"
+              >
+                <date-picker
+                  :format="'YYYY-MM-DD'"
+                  :first-day-of-week="1"
+                  :value="formatTime(project.publishTime)"
+                  @change="setProjectTime('publishTime', $event)"
+                />
+                <div class="input-group-append">
+                  <i class="icon icon-calendar"></i>
+                </div>
+              </div>
+              <span v-show="!project.publishTime" class="invalid-feedback">
+                公開日期不能留空
+              </span>
+            </div>
+            <div class="col-12 pt-2">
+              <small class="text-gray">
+                計畫內的詮釋資料將會直接公開，鑑定資訊、影像資料的<span
+                  :class="{ warning: isOverPublicLimit }"
+                  >公開日期之上限為計畫起始時間的5年後。</span
+                >
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="action">
+        <div @click="$router.back()" class="btn btn-default">取消</div>
+        <button
+          type="submit"
+          @click.stop.prevent="doDone()"
+          class="btn btn-orange"
+          :disabled="isOverPublicLimit || !this.project.publishTime"
+        >
+          {{ doneBtnText }}
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import DatePicker from 'vue2-datepicker';
 import moment from 'moment';
 
 export default {
   props: {
+    doneBtnText: String,
     project: Object,
+  },
+  components: {
+    DatePicker,
+  },
+  computed: {
+    isOverPublicLimit: function() {
+      return (
+        moment(this.project.startTime).add(5, 'years') <
+        moment(this.project.publishTime)
+      );
+    },
   },
   methods: {
     formatTime(v) {
@@ -82,6 +222,11 @@ export default {
     },
     changeProps(key, value) {
       this.$emit('change', Object.assign({}, this.project, { [key]: value }));
+    },
+    doDone() {
+      if (!this.isOverPublicLimit && this.project.publishTime) {
+        this.$emit('done');
+      }
     },
   },
 };
--- src/pages/Project/Create.vue
+++ src/pages/Project/Create.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    <h1>建立計畫</h1>
-    <div>
-      <BasicInfo :project="project" :areas="projectAreas" @change="onChange" />
-      <CcInfo :project="project" @change="onChange" />
+  <div class="page-project">
+    <div class="container">
+      <h1 class="heading">新增計畫</h1>
+      <div>
+        <BasicInfo
+          v-if="step === 1"
+          doneBtnText="下一步"
+          :project="project"
+          :areas="projectAreas"
+          @change="onChange"
+          @done="step = 2"
+        />
+        <CcInfo
+          v-if="step === 2"
+          doneBtnText="新增計畫"
+          :project="project"
+          @change="onChange"
+          @done="submitProject"
+        />
+      </div>
     </div>
-    <button @click="submitProject">新增</button>
   </div>
 </template>
 
 <script>
 import { createNamespacedHelpers } from 'vuex';
+import moment from 'moment';
 
 import BasicInfo from '@/components/ProjectEdit/BasicInfo.vue';
 import CcInfo from '@/components/ProjectEdit/CcInfo.vue';
@@ -21,22 +36,23 @@ const projects = createNamespacedHelpers('projects');
 export default {
   data: function() {
     return {
+      step: 1,
       project: {
         title: undefined,
         shortTitle: undefined,
         funder: undefined,
         code: undefined,
         principalInvestigator: undefined,
-        startTime: undefined,
-        endTime: undefined,
+        startTime: this.getTodayDate(),
+        endTime: this.plusNYears(this.getTodayDate(), 1),
         areas: [],
         description: undefined,
         note: undefined,
         coverImageFile: undefined,
-        publishTime: undefined,
-        interpretiveDataLicense: undefined,
-        identificationInformationLicense: undefined,
-        videoMaterialLicense: undefined,
+        publishTime: this.plusNYears(this.getTodayDate(), 1),
+        interpretiveDataLicense: 'cc0',
+        identificationInformationLicense: 'by',
+        videoMaterialLicense: 'cc0',
       },
     };
   },
@@ -49,6 +65,12 @@ export default {
   },
   methods: {
     ...projects.mapActions(['postProject']),
+    plusNYears(dateTimeString, n = 1) {
+      return moment(dateTimeString).add(n, 'years');
+    },
+    getTodayDate() {
+      return moment(Date.now());
+    },
     onChange(v) {
       Object.assign(this.project, v);
     },
