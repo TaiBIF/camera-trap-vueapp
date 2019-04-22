4b8e9735621394785027d9fdfcb945f46abd54cc
diabloevagto <diabloevagto@gmail.com>
2019-04-22 18:04:41 +0800

    feat: 新增側邊樣區列表

--- src/pages/Project/EditProject/CameraLocation.vue
+++ src/pages/Project/EditProject/CameraLocation.vue
@@ -1,133 +1,36 @@
 <template>
   <div>
-    <h1>計畫管理-相機位置管理</h1>
     <p v-if="errorMessage" :style="{ color: 'red' }">
       錯誤訊息: {{ errorMessage }}
     </p>
-    <div id="split-view">
-      <ul>
-        <li :key="area.id" v-for="area in studyAreas">
-          <p
-            @click="currentstudyAreaId = area.id"
-            :style="[
-              currentstudyAreaId === area.id
-                ? { color: 'red' }
-                : { color: 'black' },
-            ]"
-          >
-            {{ `${area.title} / ${area.id}` }}
-          </p>
-          <ul>
-            <li
-              :key="area.id"
-              @click="currentstudyAreaId = area.id"
-              v-for="area in area.children"
-            >
-              <p
-                @click="currentstudyAreaId = area.id"
-                :style="[
-                  currentstudyAreaId === area.id
-                    ? { color: 'red' }
-                    : { color: 'black' },
-                ]"
-              >
-                {{ `${area.title} / ${area.id}` }}
-              </p>
-            </li>
-            <li>
-              <label>新增: </label
-              ><input
-                type="text"
-                @change="addStudyArea($event.target.value, area.id)"
-              />
-            </li>
-          </ul>
-        </li>
-        <li>
-          <label>新增: </label
-          ><input type="text" @change="addStudyArea($event.target.value)" />
-        </li>
-      </ul>
-      <div>
-        <p>id: {{ currentstudyAreaId }}</p>
-        <ul>
-          <li :key="loc.id" v-for="loc in cameraLocations">
-            <button @click="deleteCameraLocation(loc.id)">X</button>
-            <span @click="currentCameraLocationId = loc.id">
-              {{ `${loc.name} / ${loc.vegetation} / ${loc.settingTime}` }}
-            </span>
-          </li>
-        </ul>
-        <div
-          :style="[
-            {
-              margin: '1rem',
-              border: '1px green solid',
-            },
-          ]"
-        >
-          <h1>新增相機位置</h1>
-          <label>相機位置名稱</label>
-          <input type="text" v-model="newCameraLocation.name" />
-          <br />
-          <label>架設日期</label>
-          <input type="date" v-model="newCameraLocation.settingTime" />
-          <br />
-          <label>緯度 (WGS84) </label>
-          <input type="text" v-model="newCameraLocation.latitude" />
-          <br />
-          <label>經度 (WGS84) </label>
-          <input type="text" v-model="newCameraLocation.longitude" />
-          <br />
-          <label>海拔（公尺） </label>
-          <input type="text" v-model="newCameraLocation.altitude" />
-          <br />
-          <label>植被</label>
-          <input type="text" v-model="newCameraLocation.vegetation" />
-          <br />
-          <label>土地覆蓋類型 </label>
-          <input type="text" v-model="newCameraLocation.landCover" />
-          <button @click="addCameraLocation" :disabled="!currentstudyAreaId">
-            新增
-          </button>
-        </div>
-        <div
-          :style="[
-            {
-              margin: '1rem',
-              border: '1px green solid',
-            },
-          ]"
-        >
-          <h1>編輯相機位置</h1>
-          <label>相機位置名稱</label>
-          <input type="text" v-model="editCameraLocation.name" />
-          <br />
-          <label>架設日期</label>
-          <input type="date" v-model="editCameraLocation.settingTime" />
-          <br />
-          <label>緯度 (WGS84) </label>
-          <input type="text" v-model="editCameraLocation.latitude" />
-          <br />
-          <label>經度 (WGS84) </label>
-          <input type="text" v-model="editCameraLocation.longitude" />
-          <br />
-          <label>海拔（公尺） </label>
-          <input type="text" v-model="editCameraLocation.altitude" />
-          <br />
-          <label>植被</label>
-          <input type="text" v-model="editCameraLocation.vegetation" />
-          <br />
-          <label>土地覆蓋類型 </label>
-          <input type="text" v-model="editCameraLocation.landCover" />
-          <button
-            @click="putCameraLocation"
-            :disabled="!currentCameraLocationId"
-          >
-            編輯
-          </button>
-        </div>
+    <div class="panel">
+      <div class="panel-heading">
+        <h4>相機位置管理</h4>
       </div>
+      <div class="panel-body camera-editor">
+        <div class="sidebar" style="width: 300px">
+          <study-area-sidebar
+            :studyAreas="studyAreas"
+            :projectId="projectId"
+            :currentStudyAreaId="currentStudyAreaId"
+            :isEditMode="true"
+            @selectArea="selectStudyArea"
+            @addArea="addStudyArea"
+          />
+        </div>
+        <div class="maintain p-0"></div>
+      </div>
+    </div>
+
+    <div class="action">
+      <div @click="$router.back()" class="btn btn-default">取消</div>
+      <button
+        type="submit"
+        @click.stop.prevent="doSubmit()"
+        class="btn btn-orange"
+      >
+        儲存設定
+      </button>
     </div>
   </div>
 </template>
@@ -136,20 +39,28 @@
 import { createNamespacedHelpers } from 'vuex';
 import moment from 'moment';
 
+import StudyAreaSidebar from '@/components/StudyAreaSidebar/StudyAreaSidebar.vue';
+
 const studyAreas = createNamespacedHelpers('studyAreas');
 
 export default {
+  components: {
+    StudyAreaSidebar,
+  },
   data: function() {
     return {
       errorMessage: undefined,
-      currentstudyAreaId: '',
-      currentCameraLocationId: '',
+      currentStudyAreaId: undefined,
+      currentCameraLocationId: undefined,
       newCameraLocation: {},
       editCameraLocation: {},
     };
   },
+  props: {
+    setLoading: Function,
+  },
   watch: {
-    currentstudyAreaId: function(val) {
+    currentStudyAreaId: function(val) {
       this.currentCameraLocationId = '';
       this.getProjectCameraLocations({
         projectId: this.projectId,
@@ -179,7 +90,11 @@ export default {
       'putProjectCameraLocations',
       'deleteProjectCameraLocations',
     ]),
+    selectStudyArea(id) {
+      this.currentStudyAreaId = id;
+    },
     async addStudyArea(title, parent) {
+      this.setLoading(true);
       try {
         await this.postProjectStudyAreas({
           id: this.projectId,
@@ -189,12 +104,13 @@ export default {
       } catch (e) {
         this.errorMessage = JSON.stringify(e);
       }
+      this.setLoading(false);
     },
     async addCameraLocation() {
       try {
         await this.postProjectCameraLocations({
           projectId: this.projectId,
-          studyAreaId: this.currentstudyAreaId,
+          studyAreaId: this.currentStudyAreaId,
           cameraLocation: {
             ...this.newCameraLocation,
             settingTime: moment(
@@ -211,7 +127,7 @@ export default {
       try {
         await this.putProjectCameraLocations({
           projectId: this.projectId,
-          studyAreaId: this.currentstudyAreaId,
+          studyAreaId: this.currentStudyAreaId,
           cameraLocationId: this.currentCameraLocationId,
           cameraLocation: {
             ...this.editCameraLocation,
@@ -229,7 +145,7 @@ export default {
       try {
         await this.deleteProjectCameraLocations({
           projectId: this.projectId,
-          studyAreaId: this.currentstudyAreaId,
+          studyAreaId: this.currentStudyAreaId,
           cameraLocationId: cameraLocationId,
         });
         this.errorMessage = '';
