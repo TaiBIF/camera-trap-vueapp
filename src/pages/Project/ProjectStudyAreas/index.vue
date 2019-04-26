0ebc46b16028fc3aef4c6eb7c49e802c56ddde51
diabloevagto <diabloevagto@gmail.com>
2019-04-26 23:07:40 +0800

    feat: annotations api

--- src/pages/Project/ProjectStudyAreas/index.vue
+++ src/pages/Project/ProjectStudyAreas/index.vue
@@ -1,30 +1,88 @@
 <template>
-  <div>
-    <h1>計畫樣區 - 檢視</h1>
-    <p>projectId: {{ projectId }}</p>
-    <p>studyAreaId: {{ studyAreaId }}</p>
-    <router-link
-      :to="{
-        name: 'projectStudyAreasEdit',
-        params: {
-          projectId,
-          studyAreaId,
-        },
-      }"
-    >
-      編輯樣區
-    </router-link>
+  <div class="maintain page-sheet p-0">
+    <div class="search-container">
+      <!-- Edit mode -->
+      <!-- Overview mode -->
+      <div class="search-content">
+        <a
+          class="btn btn-green-border btn-sm float-right"
+          v-tooltip.bottom="'將目前頁面或篩選範圍之資料輸出為 CSV 檔並下載'"
+        >
+          下載篩選結果
+        </a>
+        <h3 class="text-green mb-2">
+          {{ studyAreaTitle }}
+        </h3>
+        <hr class="my-0" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex';
+
+const studyAreas = createNamespacedHelpers('studyAreas');
+const annotations = createNamespacedHelpers('annotations');
+
 export default {
+  data() {
+    return {
+      query: {
+        cameraLocations: [],
+      },
+    };
+  },
+  mounted() {
+    this.getProjectCameraLocations({
+      projectId: this.projectId,
+      studyAreaId: this.studyAreaId,
+    });
+  },
+  watch: {
+    studyAreaId: function() {
+      this.getProjectCameraLocations({
+        projectId: this.projectId,
+        studyAreaId: this.studyAreaId,
+      });
+    },
+  },
   computed: {
+    ...studyAreas.mapGetters(['studyAreas']),
     projectId: function() {
       return this.$route.params.projectId;
     },
     studyAreaId: function() {
       return this.$route.params.studyAreaId;
+    },
+    studyAreaTitle: function() {
+      let title = '';
+      if (this.studyAreas.length > 0) {
+        this.studyAreas.forEach(v => {
+          // study area 第一層
+          if (v.id === this.studyAreaId) {
+            title = v.title;
+          } else if (v.children.length > 0) {
+            v.children.forEach(v2 => {
+              // study area 第二層
+              if (v2.id === this.studyAreaId) {
+                title = `${v.title} - ${v2.title}`;
+              }
+            });
+          }
+        });
+      }
+      return title;
+    },
+  },
+  methods: {
+    ...studyAreas.mapActions(['getProjectCameraLocations']),
+    ...annotations.mapActions(['getAnnotations']),
+    doSearch() {
+      this.getAnnotations({
+        studyAreaId: this.studyAreaId,
+        ...this.query,
+      });
     },
   },
 };
--- src/store/modules/projects/annotations.js
+++ src/store/modules/projects/annotations.js
@@ -1,12 +1,39 @@
+import idx from 'idx';
+
+import { getAnnotations } from '@/service';
+import { getLanguage } from '@/utils/i18n';
+
 // 計畫標注資訊，全放在 project 會過大
 
-const state = {};
+const state = {
+  annotations: [],
+  annotationsTotal: undefined,
+};
 
-const getters = {};
+const getters = {
+  annotations: state =>
+    state.annotations.map(v => ({
+      ...v,
+      species: {
+        ...v.species,
+        title: v.species[getLanguage()],
+      },
+    })),
+};
 
-const mutations = {};
+const mutations = {
+  setAnnotations(state, payload) {
+    state.annotations = idx(payload, _ => _.items) || [];
+    state.annotationsTotal = idx(payload, _ => _.total);
+  },
+};
 
-const actions = {};
+const actions = {
+  async getAnnotations({ commit }, query) {
+    const data = await getAnnotations(query);
+    commit('setAnnotations', data);
+  },
+};
 
 export default {
   namespaced: true,
