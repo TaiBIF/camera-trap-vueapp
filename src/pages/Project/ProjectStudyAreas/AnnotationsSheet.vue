b5628d8e85e91800c763910436d4ec45c253b276
diabloevagto <diabloevagto@gmail.com>
2019-04-28 15:46:12 +0800

    refactor: 刪除假資料

--- src/pages/Project/ProjectStudyAreas/AnnotationsSheet.vue
+++ src/pages/Project/ProjectStudyAreas/AnnotationsSheet.vue
@@ -123,7 +123,7 @@ export default {
             readOnly: true,
           },
         ],
-        data: [{ name: 'abc' }],
+        data: [],
         afterSelectionEnd: this.changeAnnotationIdx,
       },
     };
