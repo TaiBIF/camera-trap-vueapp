8aa996388c5d315dd4abcebaa487165a1e8c77da
oahehc <oahehc@gmail.com>
2019-05-03 22:11:58 +0800

    apply system notifications

--- src/components/NavHeader/NavHeader.vue
+++ src/components/NavHeader/NavHeader.vue
@@ -1,210 +1,219 @@
 <template>
-  <header>
-    <nav class="navbar navbar-expand-sm">
-      <router-link to="/" role="button" class="navbar-brand"
-        >Camara Capture</router-link
-      >
-      <button
-        v-if="!isLogin"
-        class="navbar-toggler collapsed"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        v-if="!isLogin"
-        class="collapse navbar-collapse justify-content-end"
-        id="navbarSupportedContent"
-      >
-        <div class="navbar-nav justify-content-md-end subnav">
-          <div class="nav-item dropdown help">
-            <a
-              class="nav-link dropdown-toggle"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-              >幫助</a
-            >
-            <div class="dropdown-menu dropdown-menu-right">
-              <router-link to="/help/faq" class="dropdown-item"
-                >常見問題</router-link
-              >
-              <router-link to="/help/contact-us" class="dropdown-item"
-                >聯絡我們</router-link
-              >
-              <router-link to="/help/privacy" class="dropdown-item"
-                >隱私權政策</router-link
-              >
-            </div>
-          </div>
-          <div class="divider"></div>
-          <div class="nav-item ml-3">
-            <a @click="loginModalOpen = true" class="btn btn-orange">登入</a>
-          </div>
-        </div>
-      </div>
-      <div class="collapse navbar-collapse" v-if="isLogin">
-        <ul class="navbar-nav main-nav">
-          <li class="nav-item">
-            <router-link
-              class="nav-link"
-              :class="isProjectPath ? 'active' : ''"
-              role="button"
-              to="/project/overview"
-              >計畫總覽
-              <span class="sr-only" v-if="isProjectPath">(current)</span>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link
-              class="nav-link"
-              :class="isDownloadPath ? 'active' : ''"
-              role="button"
-              to="/download/search"
-              >篩選及下載
-              <span class="sr-only" v-if="isDownloadPath">(current)</span>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link
-              class="nav-link"
-              :class="isHistoryPath ? 'active' : ''"
-              role="button"
-              to="/upload-history"
-              >上傳紀錄
-              <span class="sr-only" v-if="isHistoryPath">(current)</span>
-            </router-link>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-              >幫助</a
-            >
-            <div class="dropdown-menu dropdown-menu-right">
-              <router-link to="/help/faq" class="dropdown-item"
-                >常見問題</router-link
-              >
-              <router-link to="/help/contact-us" class="dropdown-item"
-                >聯絡我們</router-link
-              >
-              <router-link to="/help/privacy" class="dropdown-item"
-                >隱私權政策</router-link
-              >
-            </div>
-          </li>
-          <li class="divider"></li>
-        </ul>
-        <div class="navbar-nav subnav">
-          <div v-if="haveNotification" class="divider"></div>
-          <div v-if="haveNotification" class="nav-item dropdown notification">
-            <a
-              class="nav-item nav-link dropdown-toggle"
-              id="notification"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <i class="fa fa-bell"></i>
-            </a>
-            <div
-              id="notification-container"
-              class="dropdown-menu dropdown-menu-right"
-              aria-labelledby="notification"
-            >
+  <div>
+    <header>
+      <nav class="navbar navbar-expand-sm">
+        <router-link to="/" role="button" class="navbar-brand"
+          >Camara Capture</router-link
+        >
+        <button
+          v-if="!isLogin"
+          class="navbar-toggler collapsed"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          v-if="!isLogin"
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <div class="navbar-nav justify-content-md-end subnav">
+            <div class="nav-item dropdown help">
               <a
-                class="dropdown-item notification-item"
-                v-for="(msg, mid) in notifications"
-                :key="`log-msg-${mid}`"
+                class="nav-link dropdown-toggle"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                >幫助</a
               >
-                <div v-if="msg.collection == 'UploadSession'">
-                  <div class="meta text-gray date">
-                    {{ formattedDateTime(msg.modified) }} 您上傳了
-                  </div>
-                  <h5 class="text-green">
-                    <a href="#" class="link">
-                      <!-- {{msg.projectTitle}} {{msg.site}}-{{msg.subSite}} {{msg.cameraLocation}}<br /> {{msg.earliestDataDate}}-{{msg.latestDataDate}} -->
-                      <span v-html="msg.message"></span>
-                    </a>
-                  </h5>
-                  <div class="meta" v-if="msg.status == 'ERROR'">
-                    上傳內容可能有誤
-                    <!-- a class="text-green link">檢視錯誤</a -->
-                  </div>
-                  <div class="meta" v-else>
-                    上傳成功
-                  </div>
-                </div>
-                <div v-if="msg.collection == 'AbnormalData'">
-                  <div class="meta text-gray date">
-                    {{ formattedDateTime(msg.modified) }}
-                  </div>
-                  <h5 class="text-green">
-                    <a href="#" class="link">
-                      <span v-html="msg.message"></span>
-                    </a>
-                  </h5>
-                </div>
-                <div v-if="msg.collection == 'UserReport'">
-                  <div class="meta text-gray date">
-                    {{ formattedDateTime(msg.modified) }}
-                  </div>
-                  <h5 class="text-green">
-                    <a href="#" class="link">
-                      <span v-html="msg.message"></span>
-                    </a>
-                  </h5>
-                </div>
-                <div v-if="msg.collection == 'Announcement'">
-                  <div class="meta text-gray date">
-                    {{ formattedDateTime(msg.modified) }}
-                  </div>
-                  <h5 class="text-green">
-                    <a href="#" class="link">
-                      <span v-html="msg.message"></span>
-                    </a>
-                  </h5>
-                </div>
-              </a>
+              <div class="dropdown-menu dropdown-menu-right">
+                <router-link to="/help/faq" class="dropdown-item"
+                  >常見問題</router-link
+                >
+                <router-link to="/help/contact-us" class="dropdown-item"
+                  >聯絡我們</router-link
+                >
+                <router-link to="/help/privacy" class="dropdown-item"
+                  >隱私權政策</router-link
+                >
+              </div>
             </div>
-          </div>
-          <div class="divider"></div>
-          <div class="nav-item dropdown">
-            <a
-              class="nav-item nav-link dropdown-toggle"
-              id="navbarDropdownMenuLink"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              {{ userName }}
-            </a>
-            <div
-              class="dropdown-menu dropdown-menu-right"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              <router-link class="dropdown-item" to="/setting"
-                >設定</router-link
-              >
-              <a class="dropdown-item" href="#" @click="doLogout()">登出</a>
+            <div class="divider"></div>
+            <div class="nav-item ml-3">
+              <a @click="loginModalOpen = true" class="btn btn-orange">登入</a>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-    <login-modal :open="loginModalOpen" @close="loginModalOpen = false" />
-  </header>
+        <div class="collapse navbar-collapse" v-if="isLogin">
+          <ul class="navbar-nav main-nav">
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                :class="isProjectPath ? 'active' : ''"
+                role="button"
+                to="/project/overview"
+                >計畫總覽
+                <span class="sr-only" v-if="isProjectPath">(current)</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                :class="isDownloadPath ? 'active' : ''"
+                role="button"
+                to="/download/search"
+                >篩選及下載
+                <span class="sr-only" v-if="isDownloadPath">(current)</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                :class="isHistoryPath ? 'active' : ''"
+                role="button"
+                to="/upload-history"
+                >上傳紀錄
+                <span class="sr-only" v-if="isHistoryPath">(current)</span>
+              </router-link>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                >幫助</a
+              >
+              <div class="dropdown-menu dropdown-menu-right">
+                <router-link to="/help/faq" class="dropdown-item"
+                  >常見問題</router-link
+                >
+                <router-link to="/help/contact-us" class="dropdown-item"
+                  >聯絡我們</router-link
+                >
+                <router-link to="/help/privacy" class="dropdown-item"
+                  >隱私權政策</router-link
+                >
+              </div>
+            </li>
+            <li class="divider"></li>
+          </ul>
+          <div class="navbar-nav subnav">
+            <div v-if="haveNotification" class="divider"></div>
+            <div v-if="haveNotification" class="nav-item dropdown notification">
+              <a
+                class="nav-item nav-link dropdown-toggle"
+                id="notification"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i class="fa fa-bell"></i>
+              </a>
+              <div
+                id="notification-container"
+                class="dropdown-menu dropdown-menu-right"
+                aria-labelledby="notification"
+              >
+                <a
+                  class="dropdown-item notification-item"
+                  v-for="(msg, mid) in notifications"
+                  :key="`log-msg-${mid}`"
+                >
+                  <div v-if="msg.collection == 'UploadSession'">
+                    <div class="meta text-gray date">
+                      {{ formattedDateTime(msg.modified) }} 您上傳了
+                    </div>
+                    <h5 class="text-green">
+                      <a href="#" class="link">
+                        <!-- {{msg.projectTitle}} {{msg.site}}-{{msg.subSite}} {{msg.cameraLocation}}<br /> {{msg.earliestDataDate}}-{{msg.latestDataDate}} -->
+                        <span v-html="msg.message"></span>
+                      </a>
+                    </h5>
+                    <div class="meta" v-if="msg.status == 'ERROR'">
+                      上傳內容可能有誤
+                      <!-- a class="text-green link">檢視錯誤</a -->
+                    </div>
+                    <div class="meta" v-else>
+                      上傳成功
+                    </div>
+                  </div>
+                  <div v-if="msg.collection == 'AbnormalData'">
+                    <div class="meta text-gray date">
+                      {{ formattedDateTime(msg.modified) }}
+                    </div>
+                    <h5 class="text-green">
+                      <a href="#" class="link">
+                        <span v-html="msg.message"></span>
+                      </a>
+                    </h5>
+                  </div>
+                  <div v-if="msg.collection == 'UserReport'">
+                    <div class="meta text-gray date">
+                      {{ formattedDateTime(msg.modified) }}
+                    </div>
+                    <h5 class="text-green">
+                      <a href="#" class="link">
+                        <span v-html="msg.message"></span>
+                      </a>
+                    </h5>
+                  </div>
+                  <div v-if="msg.collection == 'Announcement'">
+                    <div class="meta text-gray date">
+                      {{ formattedDateTime(msg.modified) }}
+                    </div>
+                    <h5 class="text-green">
+                      <a href="#" class="link">
+                        <span v-html="msg.message"></span>
+                      </a>
+                    </h5>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="divider"></div>
+            <div class="nav-item dropdown">
+              <a
+                class="nav-item nav-link dropdown-toggle"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                {{ userName }}
+              </a>
+              <div
+                class="dropdown-menu dropdown-menu-right"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <router-link class="dropdown-item" to="/setting"
+                  >設定</router-link
+                >
+                <a class="dropdown-item" href="#" @click="doLogout()">登出</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <login-modal :open="loginModalOpen" @close="loginModalOpen = false" />
+    </header>
+    <div
+      v-if="systemAnnouncements && systemAnnouncements.length > 0"
+      class="system-announcements"
+    >
+      <div><i class="fa fa-exclamation"></i></div>
+      <span>{{ systemAnnouncements[0].message }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -215,11 +224,14 @@ import moment from 'moment';
 import LoginModal from '@/components/Modal/LoginModal';
 
 const account = createNamespacedHelpers('account');
-// const message = createNamespacedHelpers('message');
+const notifications = createNamespacedHelpers('notifications');
 
 export default {
   name: 'NavHeader',
   components: { LoginModal },
+  mounted() {
+    this.loadSystemAnnouncements();
+  },
   data() {
     return {
       loginModalOpen: false,
@@ -233,7 +245,7 @@ export default {
   },
   computed: {
     ...account.mapGetters(['userName', 'isLogin']),
-    // ...message.mapGetters(['notifications']),
+    ...notifications.mapGetters(['systemAnnouncements']),
     isProjectPath: function() {
       return this.$route && /^\/project/.test(this.$route.path);
     },
@@ -261,7 +273,7 @@ export default {
   },
   methods: {
     ...account.mapActions(['doLogout']),
-    // ...message.mapActions(['loadNotifications']),
+    ...notifications.mapActions(['loadSystemAnnouncements']),
     formattedDateTime: function(timestamp) {
       return moment(timestamp * 1000).format('YYYY-MM-DD hh:mm:ss');
     },
@@ -279,8 +291,29 @@ export default {
 </script>
 <style lang="scss">
 header {
-  position: fixed;
   top: 0;
   left: 0;
+}
+.system-announcements {
+  height: 40px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #fec9d4;
+  color: #8b8b8b;
+  & > div {
+    width: 20px;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #d80c37;
+    color: white;
+    margin: 0 10px;
+
+    & > i {
+      font-size: 8px;
+    }
+  }
 }
 </style>
