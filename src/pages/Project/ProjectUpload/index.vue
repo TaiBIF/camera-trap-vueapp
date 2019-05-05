6026dae802c335b4d76e548bc4421c980ebd5023
diabloevagto <diabloevagto@gmail.com>
2019-05-05 13:35:25 +0800

    feat: 上傳說明 modal

--- src/pages/Project/ProjectUpload/index.vue
+++ src/pages/Project/ProjectUpload/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>計畫檔案上傳</h1>
+  <div class="page-upload">
+    <div class="container">
+      <ul class="breadcrumbs">
+        <li><router-link to="/project/overview">計畫總覽 </router-link></li>
+        <li>
+          <router-link :to="`/project/${projectDetail.id}`">
+            {{ projectDetail.title }}
+          </router-link>
+        </li>
+        <li><a>檔案上傳</a></li>
+      </ul>
+    </div>
+
+    <!-- 上傳說明 -->
+    <trail-modal :open="trialModalOpen" @close="trialModalOpen = false" />
+  </div>
 </template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+
+import TrailModal from '@/components/ProjectUpload/TrialModal.vue';
+
+const projects = createNamespacedHelpers('projects');
+
+export default {
+  components: {
+    TrailModal,
+  },
+  data() {
+    return {
+      trialModalOpen: true,
+    };
+  },
+  computed: {
+    ...projects.mapGetters(['projectDetail']),
+  },
+};
+</script>
