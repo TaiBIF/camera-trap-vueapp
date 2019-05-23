018d064bdc3eb49a56a74513589a5fb2bea498f1
Kelp <kelp@phate.org>
2019-05-23 17:51:17 +0800

    Support select camera locations at the search page.

--- src/pages/Download/Search.vue
+++ src/pages/Download/Search.vue
@@ -1,34 +1,44 @@
 <template>
   <div class="container">
     <h1 class="text-green">資料篩選與下載</h1>
-    <!-- Tabs -->
-    <div class="tab" v-bind:class="{ loading: isCalcFormAggregating }">
-      <!-- Nav Tabs -->
+    <div class="tab" v-bind:class="{ loading: isLoading }">
       <ul class="nav-tab">
-        <li class="tab-item" :class="{ active: currentTab == 0 }">
-          <a role="button" @click="currentTab = 0">資料篩選</a>
+        <li class="tab-item" :class="{ active: currentTab === 'search' }">
+          <a role="button" @click="currentTab = 'search'">資料篩選</a>
         </li>
-        <li class="tab-item" :class="{ active: currentTab == 1 }">
-          <a role="button" @click="currentTab = 1">資料計算</a>
+        <li class="tab-item" :class="{ active: currentTab === 'calculate' }">
+          <a role="button" @click="currentTab = 'calculate'">資料計算</a>
         </li>
       </ul>
-      <!-- Tab Content -->
       <div class="tab-content">
-        <!-- Content: Data Filter -->
-        <div id="search" class="tab-pane" :class="{ active: currentTab == 0 }">
-          <form action="" class="form" @submit.stop.prevent="submitSearch()">
+        <div
+          id="search"
+          class="tab-pane"
+          :class="{ active: currentTab === 'search' }"
+        >
+          <form
+            action=""
+            class="form"
+            @submit.stop.prevent="submitSearchForm()"
+          >
             <h5>資料來源</h5>
-            <div class="gray-block">
+            <div
+              class="gray-block"
+              v-for="(item, index) in form.items"
+              :key="`data-form-${index}`"
+            >
               <div class="form-content">
                 <div class="row">
                   <div class="col-3">
                     <div class="form-group">
                       <label class="required">計畫名稱：</label>
                       <v-select
-                        v-model="form.projectId"
                         :options="projectOptions"
-                        :placeholder="'請選擇計畫名稱'"
-                        resetOnOptionsChange
+                        v-model="item.selected.project"
+                        :on-change="
+                          generateHandlerForProjectSelectorChange(index)
+                        "
+                        placeholder="請選擇計畫名稱"
                       />
                     </div>
                   </div>
@@ -36,21 +46,27 @@
                     <div class="form-group">
                       <label class="required">樣區：</label>
                       <v-select
-                        v-model="form.parentArea"
-                        :options="parentAreaOptions"
-                        :placeholder="'請選擇樣區'"
-                        resetOnOptionsChange
+                        :disabled="!item.options.studyAreas.length"
+                        :options="item.options.studyAreas"
+                        v-model="item.selected.studyArea"
+                        :on-change="
+                          generateHandlerForStudyAreaSelectorChange(index)
+                        "
+                        placeholder="請選擇樣區"
                       />
                     </div>
                   </div>
                   <div class="col-3">
                     <div class="form-group">
-                      <label class="required">子樣區：</label>
+                      <label>子樣區：</label>
                       <v-select
-                        v-model="form.childArea"
-                        :options="childAreaOptions"
-                        :placeholder="'請選擇子樣區'"
-                        resetOnOptionsChange
+                        :disabled="!item.options.subStudyAreas.length"
+                        :options="item.options.subStudyAreas"
+                        v-model="item.selected.subStudyArea"
+                        :on-change="
+                          generateHandlerForSubStudyAreaSelectorChange(index)
+                        "
+                        placeholder="請選擇子樣區"
                       />
                     </div>
                   </div>
@@ -58,40 +74,35 @@
                     <div class="form-group">
                       <label class="required">相機位置：</label>
                       <v-select
-                        v-model="form.camera"
-                        :options="cameraOptions"
-                        :placeholder="'請選擇相機位置'"
-                        resetOnOptionsChange
+                        :disabled="!item.options.cameraLocations.length"
+                        :options="item.options.cameraLocations"
+                        v-model="item.selected.cameraLocation"
+                        placeholder="請選擇相機位置"
                       />
                     </div>
                   </div>
                 </div>
               </div>
               <div class="form-action">
-                <a>
-                  <span class="icon-remove"></span>
-                </a>
+                <a v-if="index > 0" @click="removeFormItem(index)">
+                  <span class="icon-remove"></span
+                ></a>
               </div>
             </div>
             <div class="row">
               <div class="col-12 text-right">
-                <a class="text-green btn pr-0" @click="addFormData()">
+                <a class="text-green btn pr-0" @click="addFormItem()">
                   <span class="icon"><i class="icon-add-green"></i></span>
                   <span class="text">新增資料來源</span>
                 </a>
               </div>
             </div>
-            <div>
-              <div class="col-12">
-                <div class="form-group">
-                  <label class="required">物種：</label>
-                  <v-select
-                    v-model="calcFormOptions.species"
-                    :options="speciesOptions"
-                    multiple
-                  ></v-select>
-                </div>
-              </div>
+
+            <div class="col-12 text-right action">
+              <button type="reset" class="btn btn-green-border">
+                清空選項
+              </button>
+              <button type="submit" class="btn btn-orange">篩選</button>
             </div>
           </form>
         </div>
@@ -102,8 +113,6 @@
 
 <script>
 import { createNamespacedHelpers } from 'vuex';
-import { equals } from 'ramda';
-import { getProjectCameraLocations } from '@/service';
 import vSelect from 'vue-select';
 
 const projects = createNamespacedHelpers('projects');
@@ -114,260 +123,146 @@ export default {
   components: { vSelect },
   data() {
     return {
-      currentTab: 0,
-      isCalcFormAggregating: false,
+      isLoading: true,
+      currentTab: 'search', // "search" | "calculate"
       form: {
-        data: [],
-        projectId: '',
-        parentArea: '',
-        childArea: '',
-        cameraLocations: '',
-        camera: '',
-        species: {
-          type: Array,
-          default: () => [],
-        },
-      },
-      cameraOptions: [],
-      calcForm: {
-        type: { label: '有效照片與目擊事件', value: 'basic-calculation' },
-        project: {
-          value: '',
-          label: '',
-        },
-        site: '',
-        subSite: 'NULL',
-        species: '',
-        fromDate: '',
-        fromTime: {
-          HH: '00',
-          mm: '00',
-        },
-        toDate: '',
-        toTime: {
-          HH: '00',
-          mm: '00',
-        },
-        effectiveTimeInterval: 30,
-        camera: [],
+        items: [
+          {
+            selected: {
+              project: null,
+              studyArea: null,
+              subStudyArea: null,
+              cameraLocation: null,
+            },
+            options: {
+              studyAreas: [],
+              subStudyAreas: [],
+              cameraLocations: [],
+            },
+            studyAreas: [], // All study areas of the selected project.
+            cameraLocations: [], // All camera locations of the selected study area.
+          },
+        ],
       },
     };
-  },
-  mounted() {
-    this.getProjects();
-    this.getProjectStudyAreas(this.form.projectId);
-    this.fetchProjectCameraLocations();
-  },
-  watch: {
-    open: function(open) {
-      // load default select area, camera base on current route
-      if (open) {
-        if (this.selectedStudyAreaId && this.selectedStudyAreaId !== 'all') {
-          this.areaOptions.some(option => {
-            if (option.value === this.selectedStudyAreaId) {
-              this.form.parentArea = option;
-              return true;
-            }
-            const area = option.children.find(
-              ({ value }) => value === this.selectedStudyAreaId,
-            );
-            if (area) {
-              this.form.parentArea = option;
-              setTimeout(() => {
-                // move to next tick to wait options ready
-                this.form.childArea = area;
-              }, 0);
-              return true;
-            }
-          });
-        }
-        if (this.cameraLocations && this.cameraLocations.length > 0) {
-          this.cameraOptions = this.cameraLocations.map(({ id, name }) => ({
-            value: id,
-            label: name,
-          }));
-        }
-        if (this.selectedCameraId) {
-          this.cameraOptions.some(option => {
-            if (option.value === this.selectedCameraId) {
-              setTimeout(() => {
-                // move to next tick to wait options ready
-                this.form.camera = option;
-              }, 0);
-              return true;
-            }
-          });
-        }
-      }
-    },
-    selectedStudyAreaId() {
-      this.fetchProjectCameraLocations();
-    },
-    'form.parentArea': function(area) {
-      if (area && area.children && area.children.length === 0) {
-        this.fetchCameraLocations(area.value);
-      }
-    },
-    'form.childArea': function(area) {
-      if (area) {
-        this.fetchCameraLocations(area.value);
-      }
-    },
   },
   computed: {
     ...projects.mapState(['projects']),
     ...studyAreas.mapGetters(['studyAreas', 'cameraLocations']),
 
-    calcFormOptions() {
-      const { project } = this.calcForm;
-      const { speciesList } = project;
-
-      return {
-        species: speciesList,
-      };
-    },
-    projectId: function() {
-      return this.form.projectId;
-    },
     projectOptions() {
-      return this.projects.map(({ title, id }) => ({
-        label: title,
-        value: id,
-      }));
-    },
-    selectedStudyAreaId: function() {
-      return this.form.selectedStudyAreaId;
-    },
-    selectedCameraId: function() {
-      return this.form.selectedCameraId;
-    },
-    areaOptions() {
-      return this.studyAreas.map(({ title, id, children }) => ({
-        label: title,
-        value: id,
-        children: !children
-          ? []
-          : children.map(({ title, id }) => ({
-              label: title,
-              value: id,
-            })),
-      }));
-    },
-    parentAreaOptions() {
-      return this.areaOptions;
-    },
-    childAreaOptions() {
-      return this.form.parentArea ? this.form.parentArea.children : [];
-    },
-    projectCameraOptions: function() {
       /*
-      All camera locations of projects.
-      The first item of the result if for form.data[0]. The second item of the result if for form.data[1].
-      @returns {Array<{Array<{label: 'string', value: 'string'}>}>}
-      */
-      return this.form.data.map(data => {
-        return this.getProjectCameraOptions(
-          data.project && data.project.value,
-          data.site && data.site.value,
-          data.subSite && data.subSite.value,
-        );
-      });
-    },
-    projectSpecOptions: function() {
-      /*
-      All species of all projects.
+      All projects.
       @returns {Array<{label: 'string', value: 'string'}>}
       */
-      const species = new Set();
-      this.Projects.forEach(project => {
-        (project.speciesList || []).forEach(spec => {
-          species.add(spec);
-        });
-      });
-      return Array.from(species).map(spec => {
+      return this.projects.map(project => {
         return {
-          label: spec,
-          value: spec,
+          label: project.title,
+          value: project.id,
         };
       });
     },
-    isFormRequiredMissing() {
-      if (!this.form.parentArea || !this.form.camera) {
-        return true;
-      }
-      if (
-        this.form.parentArea &&
-        this.form.parentArea.children.length > 0 &&
-        !this.form.childAreaId
-      ) {
-        return true;
-      }
-      return false;
-    },
   },
   methods: {
-    ...projects.mapActions(['getProjects']),
+    ...projects.mapActions(['getAllProjects']),
     ...studyAreas.mapActions([
       'getProjectStudyAreas',
       'getProjectCameraLocations',
     ]),
-    async fetchCameraLocations(areaId) {
-      const { items } = await getProjectCameraLocations(this.projectId, areaId);
-      if (items) {
-        const newOptions = items.map(({ id, name }) => ({
-          value: id,
-          label: name,
-        }));
-        // only change options when camera is different, to prevent current selected camera be reset
-        if (!equals(this.cameraOptions, newOptions)) {
-          this.cameraOptions = newOptions;
-        }
-      }
+
+    generateHandlerForProjectSelectorChange(index) {
+      return () => {
+        this.form.items[index].selected.studyArea = null;
+        this.form.items[index].selected.subStudyArea = null;
+        this.form.items[index].selected.cameraLocation = null;
+        this.fetchFormOptions(index, 'project');
+      };
     },
-    fetchProjectCameraLocations() {
-      if (this.selectedStudyAreaId && this.selectedStudyAreaId !== 'all') {
-        this.getProjectCameraLocations({
-          projectId: this.projectId,
-          studyAreaId: this.selectedStudyAreaId,
-        });
-      }
+    generateHandlerForStudyAreaSelectorChange(index) {
+      return () => {
+        this.form.items[index].selected.subStudyArea = null;
+        this.form.items[index].selected.cameraLocation = null;
+        this.fetchFormOptions(index, 'studyArea');
+      };
     },
-    getProjectCameraOptions(projectId, site, subSite) {
+    generateHandlerForSubStudyAreaSelectorChange(index) {
+      return () => {
+        this.form.items[index].selected.cameraLocation = null;
+        this.fetchFormOptions(index, 'subStudyArea');
+      };
+    },
+    async fetchFormOptions(index, changed) {
       /*
-      Get camera locations of the project.
-      @param projectId {string}
-      @param site {string}
-      @param subSite {string}
-      @returns {Array<{label: 'string', value: 'string'}}>}
+      @param changed {String} "project" | "studyArea" | "subStudyArea"
        */
-      for (let index = 0; index < this.Projects.length; index += 1) {
-        const project = this.Projects[index];
-        if (project._id === projectId) {
-          const locations = new Set();
-          project.cameraLocations.forEach(cameraLocation => {
-            if (
-              cameraLocation.site === site &&
-              cameraLocation.subSite === subSite
-            ) {
-              locations.add(cameraLocation.cameraLocation);
-            }
-          });
-          return Array.from(locations).map(location => {
-            return {
-              label: location,
-              value: location,
-            };
-          });
+      const form = this.form.items[index];
+      if (changed === 'project') {
+        await this.getProjectStudyAreas(form.selected.project.value);
+        form.studyAreas = this.studyAreas;
+        form.options.studyAreas = form.studyAreas.map(x => ({
+          label: x.title,
+          value: x.id,
+        }));
+      } else if (changed === 'studyArea') {
+        if (!form.selected.studyArea) {
+          return;
         }
+
+        const parent = form.studyAreas.find(
+          x => x.id === form.selected.studyArea.value,
+        );
+        form.options.subStudyAreas = parent.children.map(x => ({
+          label: x.title,
+          value: x.id,
+        }));
+        await this.getProjectCameraLocations({
+          projectId: form.selected.project.value,
+          studyAreaId: form.selected.studyArea.value,
+        });
+        form.cameraLocations = this.cameraLocations;
+        form.options.cameraLocations = form.cameraLocations.map(x => ({
+          label: x.name,
+          value: x.id,
+        }));
+      } else if (changed === 'subStudyArea') {
+        await this.getProjectCameraLocations({
+          projectId: form.selected.project.value,
+          studyAreaId: form.selected.subStudyArea
+            ? form.selected.subStudyArea.value
+            : form.selected.studyArea.value,
+        });
+        form.cameraLocations = this.cameraLocations;
+        form.options.cameraLocations = form.cameraLocations.map(x => ({
+          label: x.name,
+          value: x.id,
+        }));
       }
-      return [];
     },
-    submit() {
-      const { camera } = this.form;
-      this.$emit('submit', {
-        cameraLocation: camera.value,
+    addFormItem() {
+      this.form.items.push({
+        selected: {
+          project: null,
+          studyArea: null,
+          subStudyArea: null,
+          cameraLocation: null,
+        },
+        options: {
+          studyAreas: [],
+          subStudyAreas: [],
+          cameraLocations: [],
+        },
+        studyAreas: [],
+        cameraLocations: [],
       });
     },
+    removeFormItem(index) {
+      this.form.items.splice(index, 1);
+    },
+    submitSearchForm() {},
+  },
+  async mounted() {
+    await this.getAllProjects({ size: 100, sort: 'title' });
+    this.isLoading = false;
   },
 };
 </script>
