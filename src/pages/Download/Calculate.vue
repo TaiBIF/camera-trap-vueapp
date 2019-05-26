7c2f166473508030ccc9d37c63c2b17e354f4c56
Kelp <kelp@phate.org>
2019-05-26 17:33:51 +0800

    Completed calculate.

--- src/pages/Download/Calculate.vue
+++ src/pages/Download/Calculate.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-fluid" :class="{ loading: isLoading }">
     <router-link to="/download">
       <small class="text-gray">
         <i class="fa fa-chevron-left"></i> 返回資料篩選及計算
@@ -10,13 +10,6 @@
 
     <div class="panel panel-project">
       <div class="float-right">
-        <a
-          class="btn btn-green-border btn-sm"
-          v-tooltip.bottom="'將計算結果輸出為 CSV 檔並下載'"
-        >
-          <span class="icon"><i class="icon-download-green"></i></span>
-          下載計算結果
-        </a>
         <button class="btn btn-orange btn-sm ml-2" @click="submitCalculate">
           計算
         </button>
@@ -34,9 +27,9 @@
               <div class="form-group">
                 <label>計畫名稱：</label>
                 <v-select
-                  v-model="currentProject"
+                  v-model="form.selectedProject"
                   :options="projectOptions"
-                  :placeholder="'請選擇計畫名稱'"
+                  placeholder="請選擇計畫名稱"
                 />
               </div>
               <div class="row">
@@ -44,24 +37,32 @@
                   <div class="form-group">
                     <label>樣區：</label>
                     <v-select
-                      v-model="currentStudyArea"
+                      v-model="form.selectedStudyArea"
                       :options="studyAreaOptions"
-                      :placeholder="'請選擇樣區名稱'"
+                      :disabled="!studyAreaOptions.length"
+                      placeholder="請選擇樣區名稱"
                     />
                   </div>
                 </div>
-                <!--div class="col-4">
+                <div class="col-4">
                   <div class="form-group">
                     <label>子樣區：</label>
+                    <v-select
+                      v-model="form.selectedSubStudyArea"
+                      :options="subStudyAreaOptions"
+                      :disabled="!subStudyAreaOptions.length"
+                      placeholder="請選擇樣區名稱"
+                    />
                   </div>
-                </div-->
+                </div>
                 <div class="col-4">
                   <div class="form-group">
                     <label>相機位置：</label>
                     <v-select
-                      v-model="currentCameraLocation"
+                      v-model="form.selectedCameraLocation"
                       :options="cameraOptions"
-                      :placeholder="'請選相機位置'"
+                      :disabled="!cameraOptions.length"
+                      placeholder="請選相機位置"
                     />
                   </div>
                 </div>
@@ -71,9 +72,9 @@
               <div class="form-group">
                 <label>物種：</label>
                 <v-select
-                  v-model="species"
+                  v-model="form.selectedSpecies"
                   :options="speciesOptions"
-                  :placeholder="'請選擇物種'"
+                  placeholder="請選擇物種"
                 />
               </div>
               <div class="row">
@@ -83,9 +84,9 @@
                     <div class="input-group-inline">
                       <div class="input-group">
                         <date-picker
-                          v-model="startDate"
-                          :placeholder="'2018-09-20'"
-                          :format="'YYYY-MM-DD'"
+                          v-model="form.startDate"
+                          placeholder="2018-09-20"
+                          format="YYYY-MM-DD"
                           :first-day-of-week="1"
                         />
                         <div class="input-group-append">
@@ -93,7 +94,10 @@
                         </div>
                       </div>
                       <div class="input-group ml-2">
-                        <vue-timepicker v-model="startTime" format="HH:mm" />
+                        <vue-timepicker
+                          v-model="form.startTime"
+                          format="HH:mm"
+                        />
                       </div>
                     </div>
                   </div>
@@ -104,9 +108,9 @@
                     <div class="input-group-inline">
                       <div class="input-group">
                         <date-picker
-                          v-model="endDate"
-                          :placeholder="'2018-09-20'"
-                          :format="'YYYY-MM-DD'"
+                          v-model="form.endDate"
+                          placeholder="2018-09-20"
+                          format="YYYY-MM-DD"
                           :first-day-of-week="1"
                         />
                         <div class="input-group-append">
@@ -114,7 +118,7 @@
                         </div>
                       </div>
                       <div class="input-group ml-2">
-                        <vue-timepicker v-model="endTime" format="HH:mm" />
+                        <vue-timepicker v-model="form.endTime" format="HH:mm" />
                       </div>
                     </div>
                   </div>
@@ -126,17 +130,17 @@
               <div class="form-group">
                 <label>有效照片判定間隔：</label>
                 <v-select
-                  v-model="validTimeInterval"
-                  :options="[2, 5, 10, 30, 60]"
-                  :placeholder="'請選擇有效時間判定間隔'"
+                  v-model="form.selectedValidTimeInterval"
+                  :options="timeIntervalOptions"
+                  placeholder="請選擇有效時間判定間隔"
                 ></v-select>
               </div>
               <div class="form-group">
                 <label>目擊事件判定間隔：</label>
                 <v-select
-                  v-model="eventTimeInterval"
-                  :options="[2, 5, 10, 30, 60]"
-                  :placeholder="'請選擇目擊事件判斷間隔'"
+                  v-model="form.selectedEventTimeInterval"
+                  :options="timeIntervalOptions"
+                  placeholder="請選擇目擊事件判斷間隔"
                 ></v-select>
               </div>
             </div>
@@ -147,13 +151,18 @@
     </div>
     <!-- end panel -->
     <div class="sheet-container">
-      <CalculationSheet ref="sheet" :calculatorData="calculator" />
+      <CalculationSheet
+        ref="sheet"
+        :calculatorData="calculator"
+        :calculateType="calculateType"
+      />
     </div>
   </div>
 </template>
 <script>
 import { createNamespacedHelpers } from 'vuex';
-import { getProjectCameraLocations } from '@/service';
+import { getCameraLocation } from '@/service';
+import { getLanguage } from '@/utils/i18n';
 import CalculationSheet from './CalculationSheet';
 import DatePicker from 'vue2-datepicker';
 import VueTimepicker from 'vue2-timepicker';
@@ -163,12 +172,6 @@ import vSelect from 'vue-select';
 const projects = createNamespacedHelpers('projects');
 const studyAreas = createNamespacedHelpers('studyAreas');
 const calculator = createNamespacedHelpers('calculator');
-
-const getTime = (day, time) => {
-  return moment(day)
-    .hour(time.HH)
-    .minute(time.mm);
-};
 
 export default {
   components: {
@@ -180,66 +183,218 @@ export default {
   data: function() {
     return {
       isLoading: true,
-      currentStudyArea: undefined,
-      currentProject: undefined,
-      currentCameraLocation: undefined,
-      species: undefined,
-      startDate: '',
-      startTime: {
-        HH: '10',
-        mm: '00',
+      isInitial: true,
+      timeIntervalOptions: [
+        { label: 2, value: 2 * 60 * 1000 },
+        { label: 5, value: 5 * 60 * 1000 },
+        { label: 10, value: 10 * 60 * 1000 },
+        { label: 30, value: 30 * 60 * 1000 },
+        { label: 60, value: 60 * 60 * 1000 },
+      ],
+      calculateType: this.$route.query.calculateType || 'oi',
+      form: {
+        selectedProject: null,
+        selectedStudyArea: null,
+        selectedSubStudyArea: null,
+        selectedCameraLocation: null,
+        selectedSpecies: null,
+        startDate: (() => {
+          if (this.$route.query.startTime) {
+            return moment(this.$route.query.startTime).format('YYYY-MM-DD');
+          }
+          return '';
+        })(),
+        startTime: (() => {
+          if (this.$route.query.startTime) {
+            const date = moment(this.$route.query.startTime);
+            return { HH: date.format('HH'), mm: date.format('mm') };
+          }
+          return { HH: '00', mm: '00' };
+        })(),
+        endDate: (() => {
+          if (this.$route.query.endTime) {
+            return moment(this.$route.query.endTime).format('YYYY-MM-DD');
+          }
+        })(),
+        endTime: (() => {
+          if (this.$route.query.endTime) {
+            const date = moment(this.$route.query.endTime);
+            return { HH: date.format('HH'), mm: date.format('mm') };
+          }
+          return { HH: '23', mm: '59' };
+        })(),
+        selectedValidTimeInterval: (() => {
+          if (this.$route.query.validTimeInterval) {
+            return {
+              label: +this.$route.query.validTimeInterval / 60 / 1000,
+              value: +this.$route.query.validTimeInterval,
+            };
+          }
+        })(),
+        selectedEventTimeInterval: (() => {
+          if (this.$route.query.eventTimeInterval) {
+            return {
+              label: +this.$route.query.eventTimeInterval / 60 / 1000,
+              value: +this.$route.query.eventTimeInterval,
+            };
+          }
+        })(),
       },
-      endDate: '',
-      endTime: {
-        HH: '10',
-        mm: '00',
-      },
-      validTimeInterval: undefined,
-      eventTimeInterval: undefined,
     };
   },
-  mounted() {
-    this.getProjects();
+  async mounted() {
+    const tasks = [this.fetchInitialFormData()];
+    if (this.$route.query.calculateType === 'ltd') {
+      tasks.push(this.getCalculateLTD(this.$route.query));
+    } else {
+      tasks.push(this.getCalculateOI(this.$route.query));
+    }
+    await Promise.all(tasks);
+
+    this.isLoading = false;
+    setTimeout(() => {
+      this.isInitial = false;
+    });
   },
   methods: {
-    ...projects.mapActions(['getProjects', 'getProjectSpecies']),
+    ...projects.mapActions(['getAllProjects', 'getProjectSpecies']),
     ...studyAreas.mapActions([
       'getProjectStudyAreas',
       'getProjectCameraLocations',
     ]),
     ...calculator.mapActions(['getCalculateOI', 'getCalculateLTD']),
-    handleStudyAreaChange() {
-      this.studyAreas.getProjectCameraLocations();
+
+    async fetchInitialFormData() {
+      const [cameraLocation] = await Promise.all([
+        getCameraLocation(this.$route.query.cameraLocation),
+        this.getAllProjects({ size: 100, sort: 'title' }),
+      ]);
+      this.form.selectedProject = {
+        label: cameraLocation.project.title,
+        value: cameraLocation.project.id,
+      };
+      this.form.selectedCameraLocation = {
+        label: cameraLocation.name,
+        value: cameraLocation.id,
+      };
+      await Promise.all([
+        this.getProjectSpecies(cameraLocation.project.id),
+        this.getProjectStudyAreas(cameraLocation.project.id),
+        this.getProjectCameraLocations({
+          projectId: cameraLocation.project.id,
+          studyAreaId: cameraLocation.studyArea.parent
+            ? cameraLocation.studyArea.parent.id
+            : cameraLocation.studyArea.id,
+        }),
+      ]);
+      const species = this.projectSpecies.find(
+        x => x.id === this.$route.query.species,
+      );
+      this.form.selectedSpecies = {
+        label: species.title[getLanguage()],
+        value: species.id,
+      };
+      if (cameraLocation.studyArea.parent) {
+        // Set the parent and the child study area.
+        const parent = this.studyAreas.find(
+          x => x.id === cameraLocation.studyArea.parent.id,
+        );
+        const child = parent.children.find(
+          x => x.id === cameraLocation.studyArea.id,
+        );
+        this.form.selectedStudyArea = {
+          label: parent.title,
+          value: parent.id,
+        };
+        this.form.selectedSubStudyArea = {
+          label: child.title,
+          value: child.id,
+        };
+      } else {
+        const parent = this.studyAreas.find(
+          x => x.id === cameraLocation.studyArea.id,
+        );
+        this.form.selectedStudyArea = {
+          label: parent.title,
+          value: parent.id,
+        };
+      }
     },
     async submitCalculate() {
+      // todo: redo again!!!!!
       this.isLoading = true;
-      await this.getCalculateOI({
-        cameraLocation: this.currentCameraLocation.value,
-        startTime: getTime(this.startDate, this.startTime).toISOString(),
-        endTime: getTime(this.endDate, this.endTime).toISOString(),
-        species: this.species.value,
-        validTimeInterval: this.validTimeInterval,
-        eventTimeInterval: this.eventTimeInterval,
-      });
+      this.isInitial = true;
+
+      const query = {
+        calculateType: this.$route.query.calculateType,
+        cameraLocation: this.form.selectedCameraLocation.value,
+        species: this.form.selectedSpecies.value,
+        validTimeInterval: this.form.selectedValidTimeInterval.value,
+        eventTimeInterval: this.form.selectedEventTimeInterval.value,
+      };
+      if (this.form.startDate) {
+        const time = new Date(this.form.startDate);
+        time.setHours(+this.form.startTime.HH);
+        time.setMinutes(+this.form.startTime.mm);
+        query.startTime = time.toISOString();
+      }
+      if (this.form.endDate) {
+        const time = new Date(this.form.endDate);
+        time.setHours(+this.form.endTime.HH);
+        time.setMinutes(+this.form.endTime.mm);
+        query.endTime = time.toISOString();
+      }
+      this.$router.push({ path: '/download/calculate', query });
+
+      const tasks = [this.fetchInitialFormData()];
+      if (this.$route.query.calculateType === 'ltd') {
+        tasks.push(this.getCalculateLTD(this.$route.query));
+      } else {
+        tasks.push(this.getCalculateOI(this.$route.query));
+      }
+      await Promise.all(tasks);
       this.isLoading = false;
+      setTimeout(() => {
+        this.isInitial = false;
+      });
     },
   },
   watch: {
-    currentStudyArea: function() {
-      this.getProjectCameraLocations({
-        projectId: this.currentProject.value,
-        studyAreaId: this.currentStudyArea.value,
-      });
+    'form.selectedProject': function() {
+      if (this.isInitial) {
+        return;
+      }
+      this.getProjectStudyAreas(this.form.selectedProject.value);
+      this.getProjectSpecies(this.form.selectedProject.value);
+      this.form.selectedSpecies = null;
     },
-    currentProject: function() {
-      this.getProjectStudyAreas(this.currentProject.value);
-      this.getProjectSpecies(this.currentProject.value);
+    'form.selectedStudyArea': function() {
+      if (this.isInitial) {
+        return;
+      }
+      this.getProjectCameraLocations({
+        projectId: this.form.selectedProject.value,
+        studyAreaId: this.form.selectedStudyArea.value,
+      });
+      this.form.selectedSubStudyArea = null;
+      this.form.selectedCameraLocation = null;
+    },
+    'form.selectedSubStudyArea': function() {
+      if (this.isInitial) {
+        return;
+      }
+      this.getProjectCameraLocations({
+        projectId: this.form.selectedProject.value,
+        studyAreaId: this.form.selectedSubStudyArea.value,
+      });
+      this.form.selectedCameraLocation = null;
     },
   },
   computed: {
     ...projects.mapState(['projects', 'projectSpecies']),
     ...studyAreas.mapGetters(['studyAreas', 'cameraLocations']),
     ...calculator.mapState(['calculator']),
+
     projectOptions() {
       return this.projects.map(({ title, id }) => ({
         label: title,
@@ -247,20 +402,29 @@ export default {
       }));
     },
     studyAreaOptions() {
-      return this.studyAreas.map(({ title, id, children }) => ({
+      return this.studyAreas.map(({ title, id }) => ({
         label: title,
         value: id,
-        children: !children
-          ? []
-          : children.map(({ title, id }) => ({
-              label: title,
-              value: id,
-            })),
+      }));
+    },
+    subStudyAreaOptions() {
+      if (!this.form.selectedSubStudyArea) {
+        return [];
+      }
+      const studyArea = this.studyAreas.find(
+        x => x.id === this.form.selectedSubStudyArea.value,
+      );
+      if (!studyArea) {
+        return [];
+      }
+      return studyArea.children.map(({ title, id }) => ({
+        label: title,
+        value: id,
       }));
     },
     speciesOptions() {
       return this.projectSpecies.map(({ title, id }) => ({
-        label: title['zh-TW'],
+        label: title[getLanguage()],
         value: id,
       }));
     },
--- src/pages/Download/CalculationSheet.vue
+++ src/pages/Download/CalculationSheet.vue
@@ -9,30 +9,69 @@
 </template>
 <script>
 import { HotTable } from '@handsontable/vue';
-import { createNamespacedHelpers } from 'vuex';
+import moment from 'moment';
+
 export default {
   props: {
-    calculatorData: Object,
+    calculateType: String,
+    calculatorData: [Object, Array],
   },
   data: function() {
     return {
       HandsontableSetting: {
         height: 500,
-        colHeaders: ['項目', '計算結果'],
+        colHeaders:
+          this.calculateType === 'oi'
+            ? ['項目', '計算結果']
+            : ['日期', '初測延遲'],
         readOnly: true,
-        data: [
-          [
-            '相機工作時數',
-            `${this.calculatorData.cameraLocationWorkDuration} 小時`,
-          ],
-          ['有效照片', `${this.calculatorData.validQuantity} 張`],
-          ['目擊事件', `${this.calculatorData.eventQuantity} 次`],
-          ['平均偵測天數比例', 'xx %'],
-          ['有效圖片除以相機工作時長', 'xx 小時'],
-          ['目擊事件除以相機工作時長', 'xx 小時'],
-        ],
+        data: [],
       },
     };
+  },
+  computed: {
+    data() {
+      if (this.calculateType === 'oi') {
+        const dutyHours = Math.floor(
+          this.calculatorData.cameraLocationWorkDuration / 60 / 60 / 1000,
+        );
+        return [
+          ['相機工作時數', `${dutyHours} 小時`],
+          ['有效照片', `${this.calculatorData.validQuantity} 張`],
+          ['目擊事件', `${this.calculatorData.eventQuantity} 次`],
+          [
+            '有效圖片除以相機工作時長',
+            `${Math.floor(
+              this.calculatorData.validQuantity / dutyHours,
+            )} 張/小時`,
+          ],
+          [
+            '目擊事件除以相機工作時長',
+            `${Math.floor(
+              this.calculatorData.eventQuantity / dutyHours,
+            )} 次/小時`,
+          ],
+        ];
+      }
+
+      return this.calculatorData.map(x => {
+        const duration = new Date(x.duration);
+        return [
+          moment(x.time).format('YY/M/D'),
+          x.duration == null
+            ? '沒有照片'
+            : `${duration.getUTCHours()} 小時 ${`${duration.getUTCMinutes()}`.padStart(
+                2,
+                '0',
+              )} 分`,
+        ];
+      });
+    },
+  },
+  watch: {
+    calculatorData() {
+      this.$refs.sheet.$data.hotInstance.loadData(this.data);
+    },
   },
   components: {
     HotTable,
