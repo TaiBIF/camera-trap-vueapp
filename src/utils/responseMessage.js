ab61df8a35d520c4960e38d4450c00163e986247
Jack Hsieh <f2e.jack@gmail.com>
2019-05-30 04:31:06 +0800

    Defined message "新增成功" for response status code 200

---
+++ src/utils/responseMessage.js
@@ -0,0 +1,17 @@
+// Define all response messages for end-user
+const getErrorMessage = status => {
+  switch (status) {
+    case '400':
+      return '操作失敗，資料內容有誤。';
+    case '401':
+      return '操作失敗，請先登入。';
+    case '403':
+      return '操作失敗，您的帳號無權限操作此步驟';
+    case '200':
+      return '新增成功';
+    default:
+      return '操作失敗，請再試一次';
+  }
+};
+
+export default getErrorMessage;
