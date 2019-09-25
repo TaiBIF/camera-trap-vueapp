4e19b435528004052c7b771557aba339da18a96d
peggy.liu <peggy.liu@brocas.com.tw>
2019-09-25 15:26:51 +0800

    Add check delete modal img

--- src/components/CameraManagement/CheckDeleteProjectCameraModal.vue
+++ src/components/CameraManagement/CheckDeleteProjectCameraModal.vue
@@ -3,6 +3,11 @@
     <div class="modal-dialog dialog-ui" role="document">
       <div class="modal-content">
         <div class="modal-body">
+          <img
+            src="/assets/common/error-img.png"
+            width="221"
+            srcset="/assets/common/error-img@2x.png"
+          />
           <h1 class="heading mt-0">您確定要刪除{{ projectCameraName }}嗎？</h1>
           <div class="text-left">
             刪除此計畫內的相機資料，往後行程管理將無法選擇此相機．
