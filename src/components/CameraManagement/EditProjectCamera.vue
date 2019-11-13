2845714056ead5771f576bedf1f928edf50e36cd
peggy.liu <peggy.liu@brocas.com.tw>
2019-11-13 10:00:06 +0800

    Modify project camera options

--- src/components/CameraManagement/EditProjectCamera.vue
+++ src/components/CameraManagement/EditProjectCamera.vue
@@ -104,9 +104,9 @@
 import idx from 'idx';
 import vSelect from 'vue-select';
 
-const batteryTypeOptions = ['鹼性電池', '充電電池'];
-const brightnessOptions = ['自動'];
-const sensitivityOptions = ['自動'];
+const batteryTypeOptions = ['鹼性電池', '鋰電池', '鎳氫電池(充電電池)'];
+const brightnessOptions = ['關', '自動', '高', '中', '低'];
+const sensitivityOptions = ['高', '中', '低'];
 
 export default {
   name: 'EditProjectCamera',
